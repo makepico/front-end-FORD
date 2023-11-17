37a009dad1fca7d58b0976303061391d7590914a
makepico <pietrofurquim@gmail.com>
2023-11-16 21:50:36 -0300

    Adicionado script com variáveis

---
+++ JavaScript/Inicio/TrabalhandoComDados/script01.js
@@ -0,0 +1,24 @@
+var fabricanteCarro = "renault"
+var modeloCarro = "logan"
+var precoCarro = 16000
+var anoCarro = 2018
+
+// console.log("carros")
+// console.log(fabricanteCarro)
+// console.log(modeloCarro)
+// console.log(precoCarro)
+// console.log(anoCarro)
+
+//fabricante: renault modelo: logan//
+
+console.log("Fabricante: " + fabricanteCarro)
+console.log("Modelo: " + modeloCarro)
+console.log("Preço: " + precoCarro)
+console.log("Ano: " + anoCarro)
+
+
+// fabricanteCarro = "Audi"
+// modeloCarro = "Q3"
+
+// // console.log(fabricanteCarro)
+// // console.log(modeloCarro)
