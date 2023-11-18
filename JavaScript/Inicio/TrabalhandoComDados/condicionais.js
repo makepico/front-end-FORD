08363a6521b00d424ff6c5df566675e027b5d2cc
makepico <pietrofurquim@gmail.com>
2023-11-17 21:40:24 -0300

    realizado exercicio sobre area do circulo

---
+++ JavaScript/Inicio/TrabalhandoComDados/condicionais.js
@@ -0,0 +1,15 @@
+// exemplo de notas
+
+const nomeAluno = "Pietro"
+const nota1 = 9
+const nota2 = 4
+const mediaAluno = (nota1 + nota2) / 2
+
+console.log (mediaAluno);
+
+if (mediaAluno >= 7) {
+    console.log(`${nomeAluno} ficou com ${mediaAluno} de média final`)
+}
+else {
+    console.log(`${nomeAluno} ficou com ${mediaAluno} de média final`)
+}
---
+++ JavaScript/Inicio/TrabalhandoComDados/desafioArea.js
@@ -0,0 +1,10 @@
+// Faça um programa que calcule a área de um círculo
+// Utilizando conceitos que já vimos anteriormente, tais como,
+// variáveis, operadores aritméticos, concatenação, etc.
+// lembre-se da estrutura dos dados (Entrada de dados, Processamento e Saída de dados)
+// Tendo em mente que o raio da circunferência é de R = 16
+
+const PI = 3.14
+var raio = 16
+var area = PI * (raio**2)
+console.log(`O círculo de raio ${raio}cm, possui área de ${area}cm²`)
